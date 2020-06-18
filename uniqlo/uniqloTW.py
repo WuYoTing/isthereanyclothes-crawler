daa68894ee3737d4a575eb0d3fed734341603435
WuTim0307 <upk.rd00003@gmail.com>
2020-06-18 17:09:22 +0800

    feat: uniqlo爬蟲第一版

--- uniqlo/uniqloTW.py
+++ uniqlo/uniqloTW.py
@@ -1,12 +1,16 @@
 import requests
+import random
+import sys
 from bs4 import BeautifulSoup
 
-res = requests.get('https://movies.yahoo.com.tw/movie_thisweek.html')
-for x in range(0, 9, 1):
-    movie_name = BeautifulSoup(res.text).select('.release_movie_name .en')
-    print(movie_name[x].text)
-    movie_time = BeautifulSoup(res.text).select('.release_movie_time')
-    print(movie_time[x].text)
-    movie_level = BeautifulSoup(res.text).select('.leveltext span')
-    print(movie_level[x].text)
-    movie_count = BeautifulSoup(res.text).select('.leveltext .count span')
+sys.path.append("..")
+from userAgent import USER_AGENT_LIST
+
+USER_AGENT = random.choice(USER_AGENT_LIST)
+headers = {'user-agent': USER_AGENT, "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7"}
+res = requests.get('https://www.uniqlo.com/tw/', headers=headers)
+BS = BeautifulSoup(res.text, "lxml")
+for category in BS.select('#navHeader #gnav_wrapper #gnav_main #navHeader a'):
+    category_url = category['href']
+    print(category_url)
+
