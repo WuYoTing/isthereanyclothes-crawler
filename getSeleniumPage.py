e5ca4dfff714ed6352b2a1ebb8eb9e314c2fa83c
WuTim0307 <upk.rd00003@gmail.com>
2020-07-06 18:04:31 +0800

    feat: 部分重構+爬取所有uq的分類

--- getSeleniumPage.py
+++ getSeleniumPage.py
@@ -12,15 +12,15 @@ sys.path.append("..")
 from userAgent import USER_AGENT_LIST
 
 
-def getProdInfo(prod_url):
-    prod_url_set = set()
-    prodUrl = prod_url
+def getSeleniumPage(url):
     user_agent = random.choice(USER_AGENT_LIST)
     chrome_options = Options()
     chrome_options.add_argument('headless')
     chrome_options.add_argument("window-size=1024,768")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-plugins")
+    chrome_options.add_argument("--disable-images")
     chrome_options.add_argument('blink-settings=imagesEnabled=false')
     chrome_options.add_argument(f'user-agent={user_agent}')
     try:
@@ -28,17 +28,15 @@ def getProdInfo(prod_url):
         driver.set_page_load_timeout(200)
         driver.set_script_timeout(200)
     except SessionNotCreatedException as SessionNotCreated:
-        print("Exception has been thrown. " + str(SessionNotCreated) + "when deal with" + prodUrl)
         raise
     try:
-        driver.get(prodUrl)
+        driver.get(url)
     except TimeoutException as Timeout:
-        print("Exception has been thrown. " + str(Timeout) + "when deal with" + prodUrl)
         raise
     try:
         page_html = driver.page_source
     except InvalidSessionIdException as InvalidSessionId:
-        print("Exception has been thrown. " + str(InvalidSessionId) + "when deal with" + prodUrl)
         raise
     driver.close()
     page = BeautifulSoup(page_html, 'lxml')
+    return page
--- uniqlo/uniqloTW.py
+++ uniqlo/uniqloTW.py
@@ -5,10 +5,14 @@ import sys
 from bs4 import BeautifulSoup
 from exceptionFormat import exceptionFormat
 from uniqlo.uniqloProdList import getProdList
+import time
+import timeit
 
 sys.path.append("..")
 from userAgent import USER_AGENT_LIST
 
+totalProdAmout = 0
+totalProdListAmout = 0
 prod_url_list_set = set()
 prod_url_set = set()
 tmp_prod_url_set = set()
@@ -19,16 +23,39 @@ BS = BeautifulSoup(res.text, "lxml")
 for category in BS.select('#navHeader .gnav_list .gnav_category #gnav_women li a'):
     category_url = category['href']
     if 'https://www.uniqlo.com/tw/store/feature/women' in category_url:
-        print(category.text + ' :' + category_url)
-        prod_url_list_set.add(category_url)
+        if 'https://www.uniqlo.com/tw/store/feature/women/featured' not in category_url and \
+                'https://www.uniqlo.com/tw/store/feature/women/special-size' not in category_url:
+            print(category.text + ' :' + category_url)
+            prod_url_list_set.add(category_url)
+for category in BS.select('#navHeader .gnav_list .gnav_category #gnav_men li a'):
+    category_url = category['href']
+    if 'https://www.uniqlo.com/tw/store/feature/men' in category_url:
+        if 'https://www.uniqlo.com/tw/store/feature/men/featured' not in category_url and \
+                'https://www.uniqlo.com/tw/store/feature/men/special-size' not in category_url:
+            print(category.text + ' :' + category_url)
+            prod_url_list_set.add(category_url)
+
+start_time = time.time()
 for prod_list_url in prod_url_list_set.copy():
     try:
         tmp_prod_url_set = getProdList(prod_list_url)
     except Exception as ec:
-        exceptionFormat(ec,prod_list_url)
+        print("Exception has been thrown. " + str(ec) + "when deal with " + prod_list_url)
+        exceptionFormat(ec, prod_list_url)
     else:
+        totalProdListAmout = totalProdListAmout + 1
         prod_url_set = prod_url_set.union(tmp_prod_url_set)
         prod_url_list_set.remove(prod_list_url)
-
+end_time = time.time()
 for prod_url in prod_url_set:
+    totalProdAmout = totalProdAmout + 1
     print(prod_url)
+print('uniqlo 共' + str(totalProdListAmout) + ' 個類別 ' + str(totalProdAmout) + '筆產品')
+prod_info_total = round(end_time - start_time, 2)
+m, s = divmod(prod_info_total, 60)
+h, m = divmod(m, 60)
+prod_info_total = "%02d:%02d:%02d" % (h, m, s)
+print('爬取' + str(totalProdListAmout) + '個類別資訊花了', prod_info_total)
+prod_info_total = round(end_time - start_time, 2) / totalProdListAmout
+print('平均時間: ' + str(prod_info_total) + '秒')
+
